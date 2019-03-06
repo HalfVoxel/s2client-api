367dab35f468fbc4419f5ee5ace054b70e208040
Aron Granberg <aron.granberg@gmail.com>
2019-03-06 01:14:47 +0100

    Add some default constructors (necessary for deserialization using cereal)

--- include/sc2api/sc2_map_info.h
+++ include/sc2api/sc2_map_info.h
@@ -57,6 +57,8 @@ struct PlayerInfo {
     Race race_actual;
     Difficulty difficulty;
 
+    PlayerInfo() {}
+
     PlayerInfo(uint32_t player_id, PlayerType player_type, Race race_requested, Race race_actual, Difficulty difficulty) :
         player_id(player_id),
         player_type(player_type),
